37b489fdb42046729e28b71a31b1fe2b3942ed0f
Argzon <argzon01@gmail.com>
2023-11-16 13:00:12 +0100

    mobile menu

--- src/components/nav/page.jsx
+++ src/components/nav/page.jsx
@@ -1,12 +1,14 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 // Internal imports
 import menuLinks from "@/data/menuLinks.json";
 import Logo from "../logo/page";
 
 function Nav() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <>
       {/* Top Bar */}
@@ -25,7 +27,10 @@ function Nav() {
         <Logo />
         {/* Menu */}
         {/* mobile icon */}
-        <div className="rounded-lg border flex items-center justify-center w-[60px] h-[60px] border-border md:hidden">
+        <div
+          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+          className="rounded-lg border flex items-center justify-center w-[60px] h-[60px] border-border md:hidden"
+        >
           <Image
             src="/assets/icons/bars.svg"
             alt="Mobile Menu"
@@ -65,6 +70,43 @@ function Nav() {
           </li>
         </ul>
       </div>
+
+      {/* Mobile menu */}
+      {mobileMenuOpen && (
+        <div className="p-6 rounded-lg bg-gray flex flex-col items-center">
+          <ul className="flex flex-col items-center gap-4 md:hidden w-full">
+            {menuLinks.map((item, i) => (
+              <li key={i}>
+                <Link
+                  className="hover:text-primary transition duration-300 block py-4 uppercase"
+                  href={item.link}
+                >
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          {/* Buttons */}
+          <ul className="md:hidden grid grid-cols-2 gap-4 w-full mt-10">
+            <li>
+              <Link
+                href="/signin"
+                className="border-darkBlack border rounded-full w-full flex items-center justify-center py-4 px-6 transition duration-300 hover:bg-darkBlack hover:text-white"
+              >
+                Sign in
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/signup"
+                className="bg-primary rounded-full py-4 px-6 flex items-center justify-center text-black hover:bg-yellow transition duration-300"
+              >
+                Register for free
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
