9874ff9406235a394e72b42120f70ea1c7c47c44
Argzon <argzon01@gmail.com>
2023-12-30 03:15:27 +0100

    Filter users logs admin

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -7,35 +7,18 @@ import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { parseISO, format } from "date-fns";
 
-const people = [
-  { id: 1, name: "Wade Cooper" },
-  { id: 2, name: "Arlene Mccoy" },
-  { id: 3, name: "Devon Webb" },
-  { id: 4, name: "Tom Cook" },
-  { id: 5, name: "Tanya Fox" },
-  { id: 6, name: "Hellen Schmidt" },
-];
-
 function AdminPage() {
-  const [selected, setSelected] = useState(people[0]);
+  const [selected, setSelected] = useState(null);
   const [query, setQuery] = useState("");
   const [userLogs, setUserLogs] = useState([]);
   const [accounts, setAccounts] = useState([]);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [peoples, setPeoples] = useState([]);
+  const [filteredUserLogs, setFilteredUserLogs] = useState([]);
 
-  console.log("User Logs:", userLogs);
-  console.log("Accounts:", accounts);
-
-  const filteredPeople =
-    query === ""
-      ? people
-      : people.filter((person) =>
-          person.name
-            .toLowerCase()
-            .replace(/\s+/g, "")
-            .includes(query.toLowerCase().replace(/\s+/g, ""))
-        );
+  // console.log("Search Results:", searchResults);
+  console.log("Filtered User Logs:", filteredUserLogs);
 
   const supabase = createClientComponentClient();
 
@@ -65,6 +48,47 @@ function AdminPage() {
     getData();
   }, [supabase]);
 
+  useEffect(() => {
+    // Define the mergeObjects function within the useEffect hook
+    const mergeObjects = (userLogs, accounts) => {
+      const mergedPeople = [];
+
+      userLogs.forEach((userLog) => {
+        const matchingAccount = accounts.find(
+          (account) => account.email === userLog.email
+        );
+        if (matchingAccount) {
+          mergedPeople.push({
+            ...userLog,
+            ...matchingAccount,
+          });
+        } else {
+          mergedPeople.push(userLog);
+        }
+      });
+
+      return mergedPeople;
+    };
+    const mergedPeople = mergeObjects(userLogs, accounts);
+    setPeoples(mergedPeople);
+
+    // Filter and update filteredUserLogs based on selected email
+    const filteredData = mergedPeople.filter((person) =>
+      selected ? person.email === selected.email : true
+    );
+    setFilteredUserLogs(filteredData);
+  }, [selected, userLogs, accounts]); // Add mergeObjects to dependencies
+
+  const filteredPeople =
+    query === ""
+      ? accounts
+      : accounts.filter((person) =>
+          person.first_name
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
   return (
     <div>
       <div className="flex flex-col gap-2 p-2">
@@ -80,7 +104,95 @@ function AdminPage() {
                 Logged in accounts are visible here.
               </p>
             </div>
+            {/* TODO: Search Input with suggestions */}
             <div className="w-full relative flex items-center">
+              <Combobox value={selected} onChange={setSelected}>
+                <div className="relative mt-1 mb-10 w-full">
+                  <div className="relative w-full cursor-default overflow-hidden rounded-lg text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+                    <Image
+                      src="/assets/icons/search.svg"
+                      height={16}
+                      width={16}
+                      alt="search"
+                      className="absolute top-8 left-8"
+                    />
+                    <Combobox.Input
+                      className="w-full py-6 pl-16 pr-32 border border-border rounded-full text-xl text-darkBlack placeholder-darkBlack"
+                      placeholder={
+                        (selected &&
+                          `${selected.first_name} ${selected.last_name}`) ||
+                        "Search users..."
+                      }
+                      onChange={(event) => {
+                        setQuery(event.target.value);
+                        event.target.value;
+                      }}
+                    />
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-2">
+                      <button className="py-3 px-4 rounded-full bg-[#D8D8D8] text-darkGray absolute right-6 duration-300 transition-all hover:bg-gray">
+                        Select
+                      </button>
+                    </div>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                    afterLeave={() => setQuery("")}
+                  >
+                    <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                      {filteredPeople.length === 0 && query !== "" ? (
+                        <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                          Nothing found.
+                        </div>
+                      ) : (
+                        filteredPeople.map((person) => (
+                          <Combobox.Option
+                            key={person.id}
+                            className={({ active }) =>
+                              `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                active
+                                  ? "bg-primary text-darkBlack"
+                                  : "text-gray-900"
+                              }`
+                            }
+                            value={person}
+                          >
+                            {({ selected, active }) => (
+                              <>
+                                <span
+                                  className={`block truncate ${
+                                    selected ? "font-medium" : "font-normal"
+                                  }`}
+                                >
+                                  {person.first_name} {person.last_name}
+                                </span>
+                                {selected ? (
+                                  <span
+                                    className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                      active
+                                        ? "text-darkBlack"
+                                        : "text-darkBlack"
+                                    }`}
+                                  >
+                                    <CheckIcon
+                                      className="h-5 w-5"
+                                      aria-hidden="true"
+                                    />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Combobox.Option>
+                        ))
+                      )}
+                    </Combobox.Options>
+                  </Transition>
+                </div>
+              </Combobox>
+            </div>
+            {/* <div className="w-full relative flex items-center">
               <Image
                 src="/assets/icons/search.svg"
                 height={16}
@@ -96,86 +208,9 @@ function AdminPage() {
               <button className="py-3 px-4 rounded-full bg-[#D8D8D8] text-darkGray absolute right-6 duration-300 transition-all hover:bg-gray">
                 Search
               </button>
-            </div>
+            </div> */}
           </div>
-          {/* TODO: Search Input with suggestions */}
-          {/* <Combobox value={selected} onChange={setSelected}>
-            <div className="relative mt-1 mb-10">
-              <div className="relative w-full cursor-default overflow-hidden rounded-lg text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-                <Image
-                  src="/assets/icons/search.svg"
-                  height={16}
-                  width={16}
-                  alt="search"
-                  className="absolute top-8 left-8"
-                />
-                <Combobox.Input
-                  className="w-full py-6 pl-16 pr-32 border border-border rounded-full text-xl text-darkBlack placeholder-darkBlack"
-                  displayValue={(person) => person.name}
-                  onChange={(event) => setQuery(event.target.value)}
-                />
-                <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-                  <ChevronUpDownIcon
-                    className="h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                </Combobox.Button>
-              </div>
-              <Transition
-                as={Fragment}
-                leave="transition ease-in duration-100"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-                afterLeave={() => setQuery("")}
-              >
-                <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-                  {filteredPeople.length === 0 && query !== "" ? (
-                    <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
-                      Nothing found.
-                    </div>
-                  ) : (
-                    filteredPeople.map((person) => (
-                      <Combobox.Option
-                        key={person.id}
-                        className={({ active }) =>
-                          `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                            active
-                              ? "bg-primary text-darkBlack"
-                              : "text-gray-900"
-                          }`
-                        }
-                        value={person}
-                      >
-                        {({ selected, active }) => (
-                          <>
-                            <span
-                              className={`block truncate ${
-                                selected ? "font-medium" : "font-normal"
-                              }`}
-                            >
-                              {person.name}
-                            </span>
-                            {selected ? (
-                              <span
-                                className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                  active ? "text-darkBlack" : "text-darkBlack"
-                                }`}
-                              >
-                                <CheckIcon
-                                  className="h-5 w-5"
-                                  aria-hidden="true"
-                                />
-                              </span>
-                            ) : null}
-                          </>
-                        )}
-                      </Combobox.Option>
-                    ))
-                  )}
-                </Combobox.Options>
-              </Transition>
-            </div>
-          </Combobox> */}
+
           {/* table */}
           <table className="table-auto w-full rounded-lg border border-border">
             <thead className="text-left">
@@ -188,32 +223,24 @@ function AdminPage() {
               </tr>
             </thead>
             <tbody>
-              {userLogs.map((user) => (
-                <tr key={user.id} className="border-b border-border">
-                  <td className="p-4 text-lg">
-                    {accounts.map((account) => (
-                      <p key={account.id}>
-                        {user.email === account.email ? account.first_name : ""}
-                      </p>
-                    ))}
-                  </td>
-                  <td className="text-lg">
-                    {accounts.map((account) => (
-                      <p key={account.id}>
-                        {user.email === account.email ? account.last_name : ""}
-                      </p>
-                    ))}
-                  </td>
-                  <td className="text-lg">{user.email}</td>
-                  <td className="text-lg">
-                    {format(
-                      parseISO(user.last_login),
-                      "d LLLL, yyyy, HH:mm:ss"
-                    )}
-                  </td>
-                  <td className="text-lg">{user.ip_address}</td>
-                </tr>
-              ))}
+              {filteredUserLogs
+                .sort((a, b) => new Date(b.last_login) - new Date(a.last_login))
+                .map((person) => (
+                  <tr key={person.id} className="border-b border-border">
+                    <td className="p-4 text-lg">
+                      {person.first_name || "N/A"}
+                    </td>
+                    <td className="text-lg">{person.last_name || "N/A"}</td>
+                    <td className="text-lg">{person.email}</td>
+                    <td className="text-lg">
+                      {format(
+                        parseISO(person.last_login),
+                        "d LLLL, yyyy, HH:mm:ss"
+                      )}
+                    </td>
+                    <td className="text-lg">{person.ip_address}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
