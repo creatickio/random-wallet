2957c789fde7c2a93da86afafe4570bfc2acff64
Argzon <argzon01@gmail.com>
2024-01-03 04:55:36 +0100

    admin trade page populated with data for active and closed trades

--- src/app/admin/accounts/[id]/page.js
+++ src/app/admin/accounts/[id]/page.js
@@ -9,6 +9,7 @@ import Image from "next/image";
 import { parseISO, format, set } from "date-fns";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Link from "next/link";
 
 export default function Profile() {
   // fetch the id
@@ -35,6 +36,7 @@ export default function Profile() {
   const [error, setError] = useState(null);
   const [deposits, setDeposits] = useState([]);
   const [withdraws, setWithdraws] = useState([]);
+  const [trades, setTrades] = useState([]);
 
   // withdraw data
   const [isWithdrawModalOpen, setIsWithdrawModalOpen] = useState(false);
@@ -227,7 +229,7 @@ export default function Profile() {
     }
   }
 
-  let [isDeleteModalOpen, setIsDeleteModalOpen] = useState(true);
+  let [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   function closeDeleteModal() {
     setIsDeleteModalOpen(false);
@@ -252,6 +254,12 @@ export default function Profile() {
         .from("withdraw")
         .select("*")
         .eq("profile", router.id);
+
+      const { data: trades } = await supabase
+        .from("trade")
+        .select("*")
+        .eq("profile", session.user.id);
+
       const { data, error } = await supabase
         .from("profile")
         .select("*")
@@ -261,6 +269,7 @@ export default function Profile() {
       const user = data;
       setWithdraws(withdraws);
       setDeposits(deposits);
+      setTrades(trades);
       setFirstName(user.first_name);
       setLastName(user.last_name);
       setEmail(user.email);
@@ -1344,49 +1353,106 @@ export default function Profile() {
                     />
                   </button>
                 </div>
-                <table className="table-auto w-full rounded-lg border border-border">
-                  <thead className="text-left">
-                    <tr className="bg-lightlightGray">
-                      <th className="p-4 font-medium text-xl">
-                        Amount inserted
-                      </th>
-                      <th className="hidden md:table-cell font-medium text-xl">
-                        Date
-                      </th>
-                      <th className="font-medium text-xl">Status</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="border-b border-border">
-                      <td className="p-4 text-lg">BTC 0.002</td>
-                      <td className="hidden md:table-cell text-lg">
-                        03 December, 2023
-                      </td>
-                      <td className="text-lg">Active</td>
-                    </tr>
-                    <tr className="border-b border-border">
-                      <td className="p-4 text-lg">BTC 0.002</td>
-                      <td className="hidden md:table-cell text-lg">
-                        03 December, 2023
-                      </td>
-                      <td className="text-lg">Active</td>
-                    </tr>
-                    <tr className="border-b border-border">
-                      <td className="p-4 text-lg">BTC 0.002</td>
-                      <td className="hidden md:table-cell text-lg">
-                        03 December, 2023
-                      </td>
-                      <td className="text-lg">Active</td>
-                    </tr>
-                    <tr className="border-b border-border">
-                      <td className="p-4 text-lg">BTC 0.002</td>
-                      <td className="hidden md:table-cell text-lg">
-                        03 December, 2023
-                      </td>
-                      <td className="text-lg">Active</td>
-                    </tr>
-                  </tbody>
-                </table>
+                {trades.length > 0 ? (
+                  <table className="table-auto w-full rounded-lg border border-border">
+                    <thead className="text-left">
+                      <tr className="bg-lightlightGray">
+                        <th className="p-4 font-medium text-xl">
+                          Amount inserted
+                        </th>
+                        <th className="hidden md:table-cell font-medium text-xl">
+                          Date
+                        </th>
+                        <th className="font-medium text-xl">Trade option</th>
+                        <th className="font-medium text-xl">Status</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {trades
+                        .sort(
+                          (a, b) =>
+                            new Date(b.created_at) - new Date(a.created_at)
+                        )
+                        .map((trade) => (
+                          <tr key={trade.id} className="border-b border-border">
+                            <td className="p-4 text-lg">
+                              <Link
+                                href={`/dashboard/trade/${trade.id}`}
+                                className="block"
+                              >
+                                BTC {trade.amount}
+                              </Link>
+                            </td>
+                            <td className="hidden md:table-cell text-lg capitalize">
+                              <Link
+                                href={`/dashboard/trade/${trade.id}`}
+                                className="block"
+                              >
+                                {format(
+                                  parseISO(trade.created_at),
+                                  "d LLLL, yyyy"
+                                )}
+                              </Link>
+                            </td>
+                            <td className="text-lg capitalize">
+                              <Link
+                                href={`/dashboard/trade/${trade.id}`}
+                                className="block"
+                              >
+                                {" "}
+                                {trade.trade_option === "ai" ? (
+                                  <span className="uppercase">
+                                    {trade.trade_option}
+                                  </span>
+                                ) : (
+                                  trade.trade_option
+                                )}
+                              </Link>
+                            </td>
+                            <td className="w-[120px]">
+                              <Link href={`/dashboard/trade/${trade.id}`}>
+                                <span
+                                  className={`text-lg flex w-fit gap-1.5
+                      ${
+                        trade.trade_status === "close"
+                          ? "bg-[#E7E9E5] px-4 py-1 rounded-lg text-darkBlack capitalize"
+                          : ""
+                      } ${
+                                    trade.trade_status === "open"
+                                      ? "bg-[#D3FFCE] px-4 py-1 rounded-lg text-darkBlack capitalize"
+                                      : ""
+                                  }`}
+                                >
+                                  {trade.trade_status === "open" ? (
+                                    <Image
+                                      src="/assets/icons/check.svg"
+                                      height={20}
+                                      width={20}
+                                      alt="Completed"
+                                    />
+                                  ) : trade.trade_status === "close" ? (
+                                    <Image
+                                      src="/assets/icons/pending.svg"
+                                      height={20}
+                                      width={20}
+                                      alt="Pending"
+                                    />
+                                  ) : (
+                                    ""
+                                  )}
+                                  {trade.trade_status}
+                                </span>
+                              </Link>
+                            </td>
+                          </tr>
+                        ))}
+                    </tbody>
+                  </table>
+                ) : (
+                  <div className="p-6 border border-border text-xl text-center rounded-lg">
+                    There&apos;s no trades made yet
+                  </div>
+                )}
               </div>
             </div>
           </div>
--- src/app/admin/trade/page.js
+++ src/app/admin/trade/page.js
@@ -1,7 +1,30 @@
 import AdminNav from "@/components/admin/nav/page";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 import React from "react";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import { parseISO, format, set } from "date-fns";
 
-export default function TradeAdmin() {
+export default async function TradeAdmin() {
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const { data } = await supabase
+    .from("profile")
+    .select("*")
+    .eq("id", session.user.id);
+
+  if (!session) {
+    redirect("/signin");
+  }
+
+  const { data: trades } = await supabase.from("trade").select("*");
+  const { data: users } = await supabase.from("profile").select("*");
+
+  const user = data[0];
   return (
     <div>
       <div className="flex flex-col gap-2 p-2">
@@ -15,8 +38,9 @@ export default function TradeAdmin() {
             See and manage all trades in one place.
           </p>
         </div>
+        {/* active trades */}
         <div className="w-full px-4 md:px-6 lg:px-8">
-          {/* active trades */}
+          {/* header */}
           <div className="flex flex-col gap-2 py-8">
             <h2 className="font-medium text-4xl text-darkBlack tracking-tighter">
               Active trades
@@ -25,6 +49,284 @@ export default function TradeAdmin() {
               See and manage all trades in one place.
             </p>
           </div>
+          {/* active trades table */}
+          {trades.length > 0 ? (
+            <table className="table-auto w-full rounded-lg border border-border">
+              <thead className="text-left">
+                <tr className="bg-lightlightGray">
+                  <th className="p-4 font-medium text-xl">First name</th>
+                  <th className="font-medium text-xl">Last name</th>
+                  <th className="font-medium text-xl">Email</th>
+                  <th className="font-medium text-xl">Amount inserted</th>
+                  <th className="font-medium text-xl">Date</th>
+                  <th className="font-medium text-xl">Trade option</th>
+                  <th className="font-medium text-xl">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {trades
+                  .sort(
+                    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+                  )
+                  .filter((trade) => trade.trade_status === "open")
+                  .map((trade) => (
+                    <tr key={trade.id} className="border-b border-border">
+                      <td className="p-4 text-lg">
+                        <Link
+                          href={`/dashboard/trade/${trade.id}`}
+                          className="block"
+                        >
+                          {users.map((user) => (
+                            <p key={user.id}>
+                              {user.id === trade.profile ? user.first_name : ""}
+                            </p>
+                          ))}
+                        </Link>
+                      </td>
+                      <td className="text-lg">
+                        <Link
+                          href={`/dashboard/trade/${trade.id}`}
+                          className="block"
+                        >
+                          {users.map((user) => (
+                            <p key={user.id}>
+                              {user.id === trade.profile ? user.last_name : ""}
+                            </p>
+                          ))}
+                        </Link>
+                      </td>
+                      <td className="text-lg">
+                        <Link
+                          href={`/dashboard/trade/${trade.id}`}
+                          className="block"
+                        >
+                          {users.map((user) => (
+                            <p key={user.id}>
+                              {user.id === trade.profile ? user.email : ""}
+                            </p>
+                          ))}
+                        </Link>
+                      </td>
+                      <td className="text-lg">
+                        <Link
+                          href={`/dashboard/trade/${trade.id}`}
+                          className="block"
+                        >
+                          BTC {trade.amount}
+                        </Link>
+                      </td>
+                      <td className="text-lg capitalize">
+                        <Link
+                          href={`/dashboard/trade/${trade.id}`}
+                          className="block"
+                        >
+                          {format(parseISO(trade.created_at), "d LLLL, yyyy")}
+                        </Link>
+                      </td>
+                      <td className="text-lg capitalize">
+                        <Link
+                          href={`/dashboard/trade/${trade.id}`}
+                          className="block"
+                        >
+                          {" "}
+                          {trade.trade_option === "ai" ? (
+                            <span className="uppercase">
+                              {trade.trade_option}
+                            </span>
+                          ) : (
+                            trade.trade_option
+                          )}
+                        </Link>
+                      </td>
+                      <td className="w-[120px]">
+                        <Link href={`/dashboard/trade/${trade.id}`}>
+                          <span
+                            className={`text-lg flex w-fit gap-1.5
+                      ${
+                        trade.trade_status === "close"
+                          ? "bg-[#E7E9E5] px-4 py-1 rounded-lg text-darkBlack capitalize"
+                          : ""
+                      } ${
+                              trade.trade_status === "open"
+                                ? "bg-[#D3FFCE] px-4 py-1 rounded-lg text-darkBlack capitalize"
+                                : ""
+                            }`}
+                          >
+                            {trade.trade_status === "open" ? (
+                              <Image
+                                src="/assets/icons/check.svg"
+                                height={20}
+                                width={20}
+                                alt="Completed"
+                              />
+                            ) : trade.trade_status === "close" ? (
+                              <Image
+                                src="/assets/icons/pending.svg"
+                                height={20}
+                                width={20}
+                                alt="Pending"
+                              />
+                            ) : (
+                              ""
+                            )}
+                            {trade.trade_status}
+                          </span>
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          ) : (
+            <div className="p-6 border border-border text-xl text-center rounded-lg">
+              There&apos;s no trades made yet
+            </div>
+          )}
+        </div>
+        {/* closed trades */}
+        <div className="w-full px-4 md:px-6 lg:px-8">
+          {/* header */}
+          <div className="flex flex-col gap-2 py-8">
+            <h2 className="font-medium text-4xl text-darkBlack tracking-tighter">
+              Completed trades
+            </h2>
+            <p className="font-light text-xl text-text">
+              See and manage all trades in one place.
+            </p>
+          </div>
+          {/* active trades table */}
+          {trades.length > 0 ? (
+            <table className="table-auto w-full rounded-lg border border-border">
+              <thead className="text-left">
+                <tr className="bg-lightlightGray">
+                  <th className="p-4 font-medium text-xl">First name</th>
+                  <th className="font-medium text-xl">Last name</th>
+                  <th className="font-medium text-xl">Email</th>
+                  <th className="font-medium text-xl">Amount inserted</th>
+                  <th className="font-medium text-xl">Date</th>
+                  <th className="font-medium text-xl">Trade option</th>
+                  <th className="font-medium text-xl">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {trades
+                  .sort(
+                    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+                  )
+                  .filter((trade) => trade.trade_status === "close")
+                  .map((trade) => (
+                    <tr key={trade.id} className="border-b border-border">
+                      <td className="p-4 text-lg">
+                        <Link
+                          href={`/dashboard/trade/${trade.id}`}
+                          className="block"
+                        >
+                          {users.map((user) => (
+                            <p key={user.id}>
+                              {user.id === trade.profile ? user.first_name : ""}
+                            </p>
+                          ))}
+                        </Link>
+                      </td>
+                      <td className="text-lg">
+                        <Link
+                          href={`/dashboard/trade/${trade.id}`}
+                          className="block"
+                        >
+                          {users.map((user) => (
+                            <p key={user.id}>
+                              {user.id === trade.profile ? user.last_name : ""}
+                            </p>
+                          ))}
+                        </Link>
+                      </td>
+                      <td className="text-lg">
+                        <Link
+                          href={`/dashboard/trade/${trade.id}`}
+                          className="block"
+                        >
+                          {users.map((user) => (
+                            <p key={user.id}>
+                              {user.id === trade.profile ? user.email : ""}
+                            </p>
+                          ))}
+                        </Link>
+                      </td>
+                      <td className="text-lg">
+                        <Link
+                          href={`/dashboard/trade/${trade.id}`}
+                          className="block"
+                        >
+                          BTC {trade.amount}
+                        </Link>
+                      </td>
+                      <td className="hidden md:table-cell text-lg capitalize">
+                        <Link
+                          href={`/dashboard/trade/${trade.id}`}
+                          className="block"
+                        >
+                          {format(parseISO(trade.created_at), "d LLLL, yyyy")}
+                        </Link>
+                      </td>
+                      <td className="text-lg capitalize">
+                        <Link
+                          href={`/dashboard/trade/${trade.id}`}
+                          className="block"
+                        >
+                          {" "}
+                          {trade.trade_option === "ai" ? (
+                            <span className="uppercase">
+                              {trade.trade_option}
+                            </span>
+                          ) : (
+                            trade.trade_option
+                          )}
+                        </Link>
+                      </td>
+                      <td className="w-[120px]">
+                        <Link href={`/dashboard/trade/${trade.id}`}>
+                          <span
+                            className={`text-lg flex w-fit gap-1.5
+                      ${
+                        trade.trade_status === "close"
+                          ? "bg-[#E7E9E5] px-4 py-1 rounded-lg text-darkBlack capitalize"
+                          : ""
+                      } ${
+                              trade.trade_status === "open"
+                                ? "bg-[#D3FFCE] px-4 py-1 rounded-lg text-darkBlack capitalize"
+                                : ""
+                            }`}
+                          >
+                            {trade.trade_status === "open" ? (
+                              <Image
+                                src="/assets/icons/check.svg"
+                                height={20}
+                                width={20}
+                                alt="Completed"
+                              />
+                            ) : trade.trade_status === "close" ? (
+                              <Image
+                                src="/assets/icons/pending.svg"
+                                height={20}
+                                width={20}
+                                alt="Pending"
+                              />
+                            ) : (
+                              ""
+                            )}
+                            {trade.trade_status}
+                          </span>
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          ) : (
+            <div className="p-6 border border-border text-xl text-center rounded-lg">
+              There&apos;s no trades made yet
+            </div>
+          )}
         </div>
       </div>
     </div>
